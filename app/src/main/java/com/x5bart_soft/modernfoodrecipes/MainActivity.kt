3c1d10bb27ac7a9f7dfeec96737f566973a18072
Pavel Kovalchuk <x5bart@yandex.ru>
2021-01-26 21:32:53 +0300

    add recipes_row_layout

--- app/src/main/java/com/x5bart_soft/modernfoodrecipes/MainActivity.kt
+++ app/src/main/java/com/x5bart_soft/modernfoodrecipes/MainActivity.kt
@@ -15,11 +15,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
 
         navController = findNavController(R.id.navHostFragment)
         val appBarConfiguration = AppBarConfiguration(
