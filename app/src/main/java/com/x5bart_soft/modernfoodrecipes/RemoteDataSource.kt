b4181b1722a8d726bbf1b0f081c154fe3f1dc3d5
Pavel Kovalchuk <x5bart@yandex.ru>
2021-01-28 00:30:23 +0300

    add RemoteDataSource function

--- app/src/main/java/com/x5bart_soft/modernfoodrecipes/RemoteDataSource.kt
+++ app/src/main/java/com/x5bart_soft/modernfoodrecipes/RemoteDataSource.kt
@@ -1,4 +1,14 @@
 package com.x5bart_soft.modernfoodrecipes
 
-class RemoteDataSource {
+import com.x5bart_soft.modernfoodrecipes.models.FoodRecipe
+import retrofit2.Response
+import java.time.temporal.TemporalQueries
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(private val foodRecepesApi: FoodRecepesApi) {
+
+    suspend fun getRecipes(queries: Map<String, String>): Response<FoodRecipe>{
+        return  foodRecepesApi.getRecipes(queries)
+    }
+
 }
