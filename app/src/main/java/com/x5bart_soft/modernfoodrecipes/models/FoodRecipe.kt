f0555dc7019a9da82b7fff352d26cdfcc2cc7c21
Pavel Kovalchuk <x5bart@yandex.ru>
2021-01-27 15:01:16 +0300

    add models

---
+++ app/src/main/java/com/x5bart_soft/modernfoodrecipes/models/FoodRecipe.kt
@@ -0,0 +1,9 @@
+package com.x5bart_soft.modernfoodrecipes.models
+
+
+import com.google.gson.annotations.SerializedName
+
+data class FoodRecipe(
+    @SerializedName("results")
+    val results: List<Result>
+)
