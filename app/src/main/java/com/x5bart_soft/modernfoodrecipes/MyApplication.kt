aa68a3a5d0a9a5bb22c70ca71a3808b5d901beaa
Pavel Kovalchuk <x5bart@yandex.ru>
2021-01-28 00:22:37 +0300

    add RemoteDataSource

---
+++ app/src/main/java/com/x5bart_soft/modernfoodrecipes/MyApplication.kt
@@ -0,0 +1,8 @@
+package com.x5bart_soft.modernfoodrecipes
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication :Application(){
+}
---
+++ app/src/main/java/com/x5bart_soft/modernfoodrecipes/RemoteDataSource.kt
@@ -0,0 +1,4 @@
+package com.x5bart_soft.modernfoodrecipes
+
+class RemoteDataSource {
+}
