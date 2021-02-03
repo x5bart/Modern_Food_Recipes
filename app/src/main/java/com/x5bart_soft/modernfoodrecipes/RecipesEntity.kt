e05d0210d4b7758ca8108a683dd3771e58fe68e2
Pavel Kovalchuk <x5bart@yandex.ru>
2021-02-03 17:17:12 +0300

    add RecipesEntity

---
+++ app/src/main/java/com/x5bart_soft/modernfoodrecipes/RecipesEntity.kt
@@ -0,0 +1,14 @@
+package com.x5bart_soft.modernfoodrecipes
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.x5bart_soft.modernfoodrecipes.models.FoodRecipe
+import com.x5bart_soft.modernfoodrecipes.util.Constants.Companion.RECIPES_TABLE
+
+@Entity(tableName = RECIPES_TABLE)
+class RecipesEntity(
+    var foodRecipe: FoodRecipe
+) {
+    @PrimaryKey(autoGenerate = false)
+    var id: Int = 0
+}
