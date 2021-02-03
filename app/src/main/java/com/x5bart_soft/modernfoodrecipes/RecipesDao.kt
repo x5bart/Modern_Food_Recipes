912eca0355e3d2eec64504e9db55ed858c240e1d
Pavel Kovalchuk <x5bart@yandex.ru>
2021-02-03 17:23:54 +0300

    add RecipesDao

---
+++ app/src/main/java/com/x5bart_soft/modernfoodrecipes/RecipesDao.kt
@@ -0,0 +1,16 @@
+package com.x5bart_soft.modernfoodrecipes
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface RecipesDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRecipes(recipesEntity: RecipesEntity)
+@Query("SELECT * FROM recipes_table ORDER BY id ASC")
+    fun readRecipes(): Flow<List<RecipesEntity>>
+}
